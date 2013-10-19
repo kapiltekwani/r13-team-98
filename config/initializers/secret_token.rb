3ef07a184e07eed8499da287c6a2ff28447d2294
pramodshinde <pramod@joshsoftware.com>
2013-10-19 08:34:29 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fmatch::Application.config.secret_token = '7987ecf2073986b57d734ac1e832a812a208e816b8de2358c8875fa70b55d329914a6dd3e8c9d5c280d17809af4356125a070ada2903466dd0f06a8eb56a7a78'
