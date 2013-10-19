777f856faaf01849292039845b0389ba4e670a88
sivagollapalli <siva@joshsoftware.com>
2013-10-19 12:24:57 +0530

    Updated db seed with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,11 @@
 "Who resemblance this guy?", "Which of your friend you think has got wrong gender?", "If you are in stuck in a room with a friend, with whom you want to be?", "Which of your friends destroys mood?", "Which of your friend flirted with teacher?", "Which of your friend has daring?", "Who will get most attention in a stripper act?", "Who is your 4AM friend?", "Who is your miscalled friend?", "Who is your papu friend?", "Who spends more money from his dad's account?", "Who is your irritating friend?", "Which of your friends do you want to meet?", "Among you friends, whose timepass is Pornmovie?"].each do |question|
    Question.create!(question_text: question)
 end
+
+[{name: 'Siva', uid: '12345', friend: ['008899', '112233', '445566', '778899']}, 
+ {name: 'Rishi', uid: '112233', friend: ['12345', '445566', '778899', '008899']},
+ {name: 'Pratik', uid: '445566', friend: ['12345', '112233', '778899', '008899']},
+ {name: 'Pramod', uid: '778899', friend: ['12345', '445566', '112233', '008899']},
+ {name: 'Kapil', uid: '008899', friend: ['12345', '445566', '112233', '778899']}].each do |user|
+  User.create!(name: user[:name], uid: user[:uid], friends: user[:friend])
+ end
