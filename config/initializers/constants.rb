e1d04286fbe20cea41beec5b86eced8215803480
pramodshinde <pramod@joshsoftware.com>
2013-10-19 15:30:30 +0530

    added app id and app secret key for staging and production

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,2 +1,7 @@
-APP_ID = "405119022947593" 
-APP_SECRET = "65d210aca20cc6560fbd407f53ef075d"
+if ["development", "test"].include?(Rails.env)
+  APP_ID = "405119022947593" 
+  APP_SECRET = "65d210aca20cc6560fbd407f53ef075d"
+else
+  APP_ID = "506848876078123" 
+  APP_SECRET = "6afefc269525dd786310e1e21fd49fac"
+end
