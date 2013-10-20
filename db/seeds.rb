1f446b3adc4b9c27fcd53fe1f7d8a81b6bb0f0ed
pramodshinde <pramod@joshsoftware.com>
2013-10-20 20:06:13 +0530

    questions images added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 i = 1
-["Whom do you want to punch?", "A boy got slapped in college who do you think he was?",
-"Who resemblance this guy?", "Which of your friend you think has got wrong gender?", "If you are in stuck in a room with a friend, with whom you want to be?", "Which of your friends destroys mood?", "Which of your friend flirted with teacher?", "Which of your friend has daring?", "Who will get most attention in a stripper act?", "Who is your 4AM friend?", "Who is your miscalled friend?", "Who is your papu friend?", "Who spends more money from his dad's account?", "Who is your irritating friend?", "Which of your friends do you want to meet?", "Among you friends, whose timepass is Pornmovie?"].each do |question|
+["Whom do you want to punch?", "A boy/girl got slapped in college who do you think he was?",
+"Who resemblance this guy?", "Which of your friend you think has got wrong gender?", "If you stuck in a room with a friend, with whom you want to be?", "Which of your friends destroys mood?", "Which of your friend flirted with teacher?", "Which of your friend has daring?", "Who will get most attention in a stripper act?", "Who is your 4AM friend?", "Who is your ready to fight friend?", "Who is your cool friend?", "Who spends more money from his dad's account?", "Who is your irritating friend?", "Which of your friends do you want to meet?", "Among you friends, whose timepass is Pornmovie?", "Who is your miscalled friend?", "Who is your papu friend?"].each do |question|
    Question.create!(question_text: question, order: i)
    i += 1
 end
+
+
