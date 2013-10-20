b89505b8c77ee61ad60742742265a902b1124b22
sivagollapalli <siva@joshsoftware.com>
2013-10-21 02:46:27 +0530

    Changed question ordering

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 i = 1
-["Whom do you want to punch?", "A boy/girl got slapped in college who do you think he was?",
-"Who resembles this guy?", "Which of your friend you think has got wrong gender?", "If you are stuck in a room with a friend, with whom you would want to?", "Which of your friends always destroy mood?", "Which of your friend flirted with teacher?", "Who isyour of your friend has daring?", "Who will get most attention in a stripper act?", "Who is your 4AM friend?", "Who is your ready to fight friend?", "Who is your cool friend?", "Who spends more money from his dad's account?", "Who is your irritating friend?", "Which of your friends do you want to meet?", "Among you friends, whose timepass is Pornmovie?", "Who is your miss-call friend?", "Who is your pappu friend?", "Who is your Scholar friend?"].each do |question|
+["Whom do you want to punch?", "A boy/girl got slapped in college, who do you think he was?",
+"Who resembles this guy?", "Which of your friend you think should be of opposite gender?", "If you are stuck in a room with a friend, who it should be?", "Which of your friends always spoils your mood?", "Which of your friends has flirted with a teacher?", "Which of your friends is the most daring?", "Who is your 4AM friend?", "Who is your ready-to-fight friend?", "Who is your cool friend?", "Who spends more of his dad's money?", "Who is your most irritating friend?", "Which of your old friends do you want to meet now?", "Who is your missed-call friend?", "Who is your pappu friend?", "Who is your Scholar friend?"].each do |question|
    Question.create!(question_text: question, order: i)
    i += 1
 end
